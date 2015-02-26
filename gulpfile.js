4ad31cfeb4f3ef5d981c94ad116668b575f85ad2
mateoclarke <code@mateoclarke.com>
2015-02-26 11:17:09 -0600

    Install gulp and gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
+	gulpUtil = require('gulp-util');
+
+gulp.task('log', function() {
+  gulpUtil.log('Gulp is working...')
+});
 
 gulp.task('default', function() {
   // place code for your default task here
