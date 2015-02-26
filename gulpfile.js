5be2c05099af7d67f27d61f1b6e581f21da2a22f
mateoclarke <code@mateoclarke.com>
2015-02-26 11:28:39 -0600

    Add concatination task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/*.coffee']
+var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = [
+	'components/scripts/*'
+];
 
 gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-  	.pipe(coffee({ bare: true })
-  		.on('error', gutil.log))
-  	.pipe(gulp.dest('components/scripts'))
+  	gulp.src(coffeeSources)
+	  	.pipe(coffee({ bare: true })
+	  		.on('error', gutil.log))
+	  	.pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('components/scripts'))
+});
+
 
 gulp.task('default', function() {
   // place code for your default task here
