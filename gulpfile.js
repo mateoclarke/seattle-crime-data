c9ead79b400201527a70219419b611381e9cc184
mateoclarke <code@mateoclarke.com>
2015-02-26 12:02:57 -0600

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,11 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
-var jsSources = [
-	'components/scripts/*'
-];
+var jsSources = ['components/scripts/script.js', 'components/scripts/tagline.js'];
 var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
@@ -23,6 +22,7 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('components/scripts'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function(){
@@ -34,6 +34,7 @@ gulp.task('compass', function(){
 		}))
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
@@ -42,5 +43,12 @@ gulp.task('watch', function(){
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
+gulp.task('connect', function(){
+	connect.server({
+		root: '/builds/development/',
+		livereload: true
+	});
+});
 
-gulp.task('default', ['coffee','js','compass', 'watch']);
+
+gulp.task('default', ['coffee','js','compass', 'connect', 'watch']);
